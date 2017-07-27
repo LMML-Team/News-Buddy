8bc83ff6dd40e2603fe6477411c46572dfe2e4df
CandyMandy28 <amanda.wang294@gmail.com>
2017-07-27 16:51:02 -0400

    things are edited

--- collect_rss.py
+++ collect_rss.py
@@ -4,16 +4,22 @@ import pickle
 import requests
 import sys
 
+face_data = OrderedDict()
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "news.pickle"), 'rb') as f:
+    news_data = pickle.load(f)
+
 def get_text(link):
     """
+    Given an URL link, return the text for the stored article
     """
     response = requests.get(link)
     paragraphs = justext.justext(response.content, justext.get_stoplist("English"))
     text = "\n\n".join([p.text for p in paragraphs if not p.is_boilerplate])
     return text
 
-def collect(url, filename, limit):
+def collect(url, limit, filename="news.pickle"):
     """
+    Saves the articles of an article into a pickle file that you can specify
     """
     # read RSS feed
     d = feedparser.parse(url)
