02504f8fa2d2bec292e44f51f96695a1f5956846
Anthony Cavallaro <ajcavallaro10@gmail.com>
2017-07-28 13:56:14 -0400

    Added docs

--- name_entities.py
+++ name_entities.py
@@ -3,18 +3,32 @@ from nltk.tokenize import word_tokenize
 
 def tokenize(doc):
     """
+    Takes in a document, returns the individual words (no punctuation) in it
     
+    Parameters
+    --------------
+    doc[str]: One document as a long string
     
+    Returns
+    --------------
+    tokens[list[str]]: Document broken up into individual words
     
     """
 
     tokens = nltk.word_tokenize(doc)
-    return tokens()
+    return tokens
 
 def chunk(tokens):
     """
+    Takes in tokens, marks them by POS, finds NEs, returns consolidated list of NEs
     
+    Parameters
+    --------------
+    tokens[list[str]]: Document broken up into individual words
     
+    Returns
+    --------------
+    NEs[list[str]]: Named entities as items in a list of strings
     
     """
     entity_list = []
@@ -29,5 +43,17 @@ def chunk(tokens):
     return [' '.join(next(zip(*l))) for l in entity_list]
 
 def add_entities(doc):
+    """
+    Takes in a document, returns the named entities in that document
+    
+    Parameters
+    --------------
+    doc[str]: One document as a long string
+    
+    Returns
+    --------------
+    NEs[list[str]]: Named entities as items in a list of strings
+    
+    """
     tokens = tokenize(doc)
     return (chunk(tokens))
