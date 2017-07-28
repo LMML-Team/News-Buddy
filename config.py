fb74a8af5825d0c9b432c3f4a6f0b82de167e2c1
CandyMandy28 <amanda.wang294@gmail.com>
2017-07-28 09:58:33 -0400

    edits

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ import numpy as np
 import string
 import time
 
+# OrderDict[]:
 news_data = OrderedDict()
 with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "news.pickle"), 'rb') as f:
     news_data = pickle.load(f)
@@ -19,11 +20,16 @@ with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "news.pickle
 def save():
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "face_data.pickle"), 'wb') as f:
         pickle.dump(news_data, f, pickle.HIGHEST_PROTOCOL)
+    
 
 def remove(link):
     if not link in news_data:
             raise KeyError("document with id [" + id + "] not found in index.")
     news_data.remove(link)
+
+####################################
+######### getting the text #########
+####################################
 
 def get_text(link):
     """
@@ -54,14 +60,19 @@ def collect(url, limit, filename="news.pickle"):
     texts = {}
     for entry in d["entries"][:limit]:
         link = entry["link"]
-        if not link in self.raw_text:
+        if not link in raw_text:
             raise KeyError("document with id [" + id + "] not found in index.")
         print("downloading: " + link)
         text = get_text(link)
         texts[link] = text
+        news_data[link] = text
     
     # pickle
     pickle.dump(texts, open(filename, "wb"))
+
+############################################
+############## organize terms ##############
+############################################
 
 def tokenize(text):
     """ Converts text into tokens (also called "terms" or "words").
@@ -92,6 +103,72 @@ def tokenize(text):
 
     # lowercase and filter out punctuation (as in string.punctuation)
     return [token.lower() for token in tokens if not token in string.punctuation]
+
+def get_matches_OR(terms):
+    """ Returns set of documents that contain at least one of the specified terms.
+
+            Parameters
+            ----------
+            terms: iterable(str)
+                An iterable of terms to match on, e.g., ["cat", "hat"].
+
+            Returns
+            -------
+            set(str)
+                A set of ids of documents that contain at least one of the term.
+        """
+    # initialize set of ids to empty set
+    ids = set()
+
+    # union ids with sets of ids matching any of the terms
+    for term in terms:
+        ids.update(inverted_index[term])
+
+    return ids
+
+def get_matches_AND(terms):
+    """ Returns set of documents that contain all of the specified terms.
+
+            Parameters
+            ----------
+            terms: iterable(str)
+                An iterable of terms to match on, e.g., ["cat", "hat"].
+
+            Returns
+            -------
+            set(str)
+                A set of ids of documents that contain each term.
+        """ 
+    # initialize set of ids to those that match first term
+    ids = inverted_index[terms[0]]
+
+    # intersect with sets of ids matching rest of terms
+    for term in terms[1:]:
+        ids = ids.intersection(inverted_index[term])
+
+        return ids
+
+def get_matches_NOT(terms):
+    """ Returns set of documents that don't contain any of the specified terms.
+
+            Parameters
+            ----------
+            terms: iterable(str)
+                An iterable of terms to avoid, e.g., ["cat", "hat"].
+
+            Returns
+            -------
+            set(str)
+                A set of ids of documents that don't contain any of the terms.
+        """
+    # initialize set of ids to all ids
+    ids = set(raw_text.keys())
+
+    # subtract ids of docs that match any of the terms
+    for term in terms:
+        ids = ids.difference(inverted_index[term])
+
+    return ids
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
