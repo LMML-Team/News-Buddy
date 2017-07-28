aca2e432c19c5164073a0d215fb3edf7e028d7b4
impostercafe <mlai8088@gmail.com>
2017-07-28 09:39:31 -0400

    Optimized database storage and added a read method

--- entity_database.py
+++ entity_database.py
@@ -1,17 +1,27 @@
 import pickle
-from collections import defaultdict, Counter
+from collections import Counter
 
-def chunks_to_db(chunks, database=None):
+
+def chunks_to_db(chunks, database):
     """
     Takes in chunks that represent keywords from a document and creates/updates a dictionary that associates entities
     with keywords.
 
     Parameters
     ----------
+    chunks: iter[str]
+        Iterable of chunks from a single document
+    database: Counter
+        Existing database of counts of entities.
 
-
+    Returns
+    -------
+    entity_db: Counter
+        Database of entities' associations.
     """
-    pass
+    database.update(chunks)
+
+    return database
 
 
 def store_database(database, filename="entity_db"):
@@ -26,3 +36,21 @@ def store_database(database, filename="entity_db"):
         Path to and file name of the desired save location
     """
     pickle.dump(database, open(filename + ".pickle", "wb"))
+
+
+def read_database(filename):
+    """
+    Takes a file of a database and returns a Counter.
+
+    Parameters
+    ----------
+    filename: str
+        Path and file name of database save location
+
+    Returns
+    -------
+    Counter:
+        Counter of the database
+
+    """
+    return pickle.load(open(filename, "rb"))
