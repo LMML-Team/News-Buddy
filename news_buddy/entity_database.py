ee8d6ba6cb77ad2d4619cc14478f1a8145cd2661
impostercafe <mlai8088@gmail.com>
2017-07-31 10:43:06 -0400

    Made entity_database a class

--- news_buddy/entity_database.py
+++ news_buddy/entity_database.py
@@ -1,105 +1,85 @@
 import pickle
-from collections import Counter
+from collections import Counter, defaultdict
 
 
-def chunks_to_db(doc_id, chunks, database):
-    """
-    Takes in chunks that represent keywords from a document and updates a dictionary that associates
-    document IDs with a Counter of their entities.
+class EntityDatabase:
+    def __init__(self):
+        self.database = defaultdict(Counter)
 
-    Parameters
-    ----------
-    doc_id:
-        String of the document ID containing the chunks.
-    chunks: iter[str]
-        Iterable of chunks from the document "doc_id"
-    database: defaultdict{str: Counter}
-        Dictionary containing count of entities per each document
+    def chunks_to_db(self, doc_id, chunks):
+        """
+        Takes in chunks that represent keywords from a document and updates a dictionary that associates
+        document IDs with a Counter of their entities.
 
-    Returns
-    -------
-    entity_db: defaultdict{str: Counter}
-        Dictionary containing count of entities per each document
-    """
-    database[doc_id].update(chunks)
+        Parameters
+        ----------
+        doc_id:
+            String of the document ID containing the chunks.
+        chunks: iter[str]
+            Iterable of chunks from the document "doc_id"
+        """
+        self.database[doc_id].update(chunks)
 
-    return database
+    def store_database(self, filename="entity_db"):
+        """
+        Stores the database in a pickle file in binary mode as filename.pickle.
 
+        Parameters
+        ----------
+        filename: str
+            Path to and file name of the desired save location.
+        """
+        pickle.dump(self.database, open(filename + ".pickle", "wb"))
 
-def store_database(database, filename="entity_db"):
-    """
-    Given a database, stores it in a pickle file in binary mode as filename.pickle.
+    def read_database(self, filename):
+        """
+        Returns a Counter of the database that was store in a pickle file.
 
-    Parameters
-    ----------
-    database: dict
-        Entity database.
-    filename: str
-        Path to and file name of the desired save location.
-    """
-    pickle.dump(database, open(filename + ".pickle", "wb"))
+        Parameters
+        ----------
+        filename: str
+            Path and file name of database save location.
 
+        """
+        self.database = pickle.load(open(filename, "rb"))
 
-def read_database(filename):
-    """
-    Takes a file of a database and returns a Counter.
+    def get_top_associations(self, entity, k=2):
+        """
+        Returns the top k associations for a given entity. Currently requires exact spelling and cases.
 
-    Parameters
-    ----------
-    filename: str
-        Path and file name of database save location.
+        Parameters
+        ----------
+        entity: str
+            Entity for associations to be returned.
+        k: int
+            Number of top associations desired.
 
-    Returns
-    -------
-    defaultdict{str: Counter}
-        Entity database.
+        Returns
+        -------
+        Tuple(str):
+            Tuple of top associations with entity.
+        """
 
-    """
-    return pickle.load(open(filename, "rb"))
+        associations = Counter()
+        for counter in self.database.values():
+            if entity in counter.keys():
+                associations.update(counter)
 
+        associations.pop(entity)
+        return list(zip(*associations.most_common(k)))[0]
 
-def get_top_associations(entity, database, k=2):
-    """
-    Returns the top k associations for a given entity. Currently requires exact spelling and cases.
+    def get_top_associations_document(self, doc_id, k=2):
+        """
+        Returns the top k entities from one given documents
 
-    Parameters
-    ----------
-    entity: str
-        Entity for associations to be returned.
-    database: dict{str: Counter}
-        Database of associations between entities.
-    k: int
-        Number of top associations desired.
+        Parameters
+        ----------
+        doc_id: str
+            Desired document ID
 
-    Returns
-    -------
-    Tuple(str):
-        Tuple of top associations with entity.
-    """
-
-    associations = Counter()
-    for counter in database.values():
-        if entity in counter.keys():
-            associations.update(counter)
-
-    associations.pop(entity)
-    return list(zip(*associations.most_common(k)))[0]
-
-
-def get_top_associations_document(doc_id, database, k=2):
-    """
-    Returns the top k entities from one given documents
-
-    Parameters
-    ----------
-    doc_id: str
-        Desired document ID
-    database: defaultdict{str: Counter}
-        Database of entities in document ids
-
-    Returns
-    -------
-    Tuple(str):
-        Tuple of the top k entities of a document
-    """
-    return list(database[doc_id].most_common(k))
+        Returns
+        -------
+        Tuple(str):
+            Tuple of the top k entities of a document
+        """
+        return list(self.database[doc_id].most_common(k))
