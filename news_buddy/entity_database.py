4a6e3451e89df81fc417d078d8c6b28040f760c9
Anthony Cavallaro <ajcavallaro10@gmail.com>
2017-08-02 14:41:48 -0400

    Added docs

--- news_buddy/entity_database.py
+++ news_buddy/entity_database.py
@@ -18,6 +18,8 @@ class EntityDatabase:
         chunks: iter[str]
             Iterable of chunks from the document "doc_id"
         """
+
+        # Adds chunks from document to database dictionary. doc_id as key, chunks iter as value
         self.database[doc_id].update(chunks)
 
     def store_database(self, filename="entity_db"):
@@ -29,6 +31,8 @@ class EntityDatabase:
         filename: str
             Path to and file name of the desired save location.
         """
+
+        # Stores the database in a pickle file in binary mode as filename.pickle
         pickle.dump(self.database, open(filename + ".pickle", "wb"))
 
     def read_database(self, filename):
@@ -41,6 +45,8 @@ class EntityDatabase:
             Path and file name of database save location.
 
         """
+
+        # Returns a Counter of the database that was store in a pickle file.
         self.database = pickle.load(open(filename, "rb"))
 
     def get_top_associations(self, entity, k=2):
@@ -59,27 +65,36 @@ class EntityDatabase:
         Tuple(str):
             Tuple of top associations with entity.
         """
-
+        # Initialize counter of associations for the given entity
         associations = Counter()
+
+        # If entity is in a document's Counter, add it to associations.
         for counter in self.database.values():
             if entity in counter.keys():
                 associations.update(counter)
 
+        # Removes entity from its own associations.
         associations.pop(entity)
+
         return list(zip(*associations.most_common(k)))[0]
 
     def get_top_associations_document(self, doc_id, k=2):
         """
-        Returns the top k entities from one given documents
+        Returns the top k entities from one given document
 
         Parameters
         ----------
         doc_id: str
             Desired document ID
+            
+        k: int (default = 2)
+            Desired number of entities returned
 
         Returns
         -------
         Tuple(str):
             Tuple of the top k entities of a document
         """
+
+        # Returns list of k most common entities in a given document
         return list(self.database[doc_id].most_common(k))
